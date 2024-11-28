e4f47644187a8206021ad716f0d215f309eac9e0
Rodrigo Harder <rodrigo.harder@alura.com.br>
2024-11-28 09:50:56 -0300

    Adicionando e alterando alguns elementos

--- script.js
+++ script.js
@@ -2,16 +2,44 @@ const imagensDoCarrossel = document.querySelector('.imagens-carrossel');
 const totalDeSlides = imagensDoCarrossel.children.length;
 const botaoAnterior = document.querySelector('.anterior');
 const botaoProximo = document.querySelector('.proximo');
+const indicadoresContainer = document.querySelector('.indicadores');
 let indiceAtual = 0;
 
-function atualizarSlide() {
-  const desvio = -indiceAtual * 800;
-  imagensDoCarrossel.style.transform = `translateX(${desvio}px)`;
+function criarIndicadores() {
+  for (let i = 0; i < totalDeSlides; i++) {
+    const indicador = document.createElement('div');
+    indicador.classList.add('indicador');
+    if (i === 0) indicador.classList.add('ativo');
+    indicadoresContainer.appendChild(indicador);
+    indicador.addEventListener('click', () => {
+      indiceAtual = i;
+      atualizarSlide();
+    });
+  }
 }
+
+function atualizarIndicadores() {
+  const indicadores = document.querySelectorAll('.indicador');
+  indicadores.forEach((indicador, index) => {
+    if (index === indiceAtual) {
+      indicador.classList.add('ativo');
+    } else {
+      indicador.classList.remove('ativo');
+    }
+  });
+}
+
+function atualizarSlide() {
+  const offset = -indiceAtual * 800;
+  imagensDoCarrossel.style.transform = `translateX(${offset}px)`;
+  atualizarIndicadores();
+}
+
 function mostrarProximoSlide() {
   indiceAtual = (indiceAtual + 1) % totalDeSlides;
   atualizarSlide();
 }
+
 function mostrarSlideAnterior() {
   indiceAtual = (indiceAtual - 1 + totalDeSlides) % totalDeSlides;
   atualizarSlide();
@@ -19,4 +47,7 @@ function mostrarSlideAnterior() {
 
 botaoProximo.addEventListener('click', mostrarProximoSlide);
 botaoAnterior.addEventListener('click', mostrarSlideAnterior);
-setInterval(mostrarProximoSlide, 5000);
+
+setInterval(mostrarProximoSlide, 3000);
+
+criarIndicadores();
